e1a6515d77909a0a7a2deb53bc59b6e9dc77b354
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-03-24 16:29:22 +0100

    Show user downloads in message overview

--- src/Twig/HelperExtension.php
+++ src/Twig/HelperExtension.php
@@ -3,10 +3,13 @@
 namespace App\Twig;
 
 use App\Entity\Message;
+use App\Entity\User;
+use App\Filesystem\MessageFilesystem;
 use App\Message\DismissedMessagesHelper;
 use App\Message\MessageConfirmationHelper;
 use App\Utils\ColorUtils;
 use App\Utils\RefererHelper;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -16,19 +19,24 @@ class HelperExtension extends AbstractExtension {
     private $dismissedHelper;
     private $redirectHelper;
     private $colorUtils;
+    private $messageFilesystem;
+    private $tokenStorage;
 
     public function __construct(MessageConfirmationHelper $confirmationHelper, DismissedMessagesHelper $dismissedHelper,
-                                RefererHelper $redirectHelper, ColorUtils $colorUtils) {
+                                RefererHelper $redirectHelper, ColorUtils $colorUtils, MessageFilesystem $messageFilesystem, TokenStorageInterface $tokenStorage) {
         $this->confirmationHelper = $confirmationHelper;
         $this->dismissedHelper = $dismissedHelper;
         $this->redirectHelper = $redirectHelper;
         $this->colorUtils = $colorUtils;
+        $this->messageFilesystem = $messageFilesystem;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function getFunctions() {
         return [
             new TwigFunction('is_confirmed', [ $this, 'isConfirmed' ]),
             new TwigFunction('is_dismissed', [ $this, 'isDismissed' ]),
+            new TwigFunction('message_downloads', [ $this, 'messageDownloads' ]),
             new TwigFunction('referer_path', [ $this, 'refererPath' ]),
             new TwigFunction('foreground', [ $this, 'foregroundColor' ])
         ];
@@ -40,6 +48,22 @@ class HelperExtension extends AbstractExtension {
 
     public function isDismissed(Message $message) {
         return $this->dismissedHelper->isMessageDismissed($message);
+    }
+
+    public function messageDownloads(Message $message) {
+        $token = $this->tokenStorage->getToken();
+
+        if($token === null) {
+            return [ ];
+        }
+
+        $user = $token->getUser();
+
+        if($user instanceof User) {
+            return $this->messageFilesystem->getUserDownloads($message, $user);
+        }
+
+        return [ ];
     }
 
     public function refererPath(array $mapping, string $route, array $parameters = [ ]): string {

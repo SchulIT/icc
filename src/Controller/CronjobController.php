36cc9e288c77e64d85d6d20be9cf4217ab16af3b
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-06-25 20:47:44 +0200

    Adds ability to see cronjob results

--- src/Controller/CronjobController.php
+++ src/Controller/CronjobController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
+use Shapecode\Bundle\CronBundle\Entity\CronJob;
+use Shapecode\Bundle\CronBundle\Entity\CronJobResult;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
@@ -11,12 +14,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @IsGranted("ROLE_CRON")
- */
 class CronjobController extends AbstractController {
 
     /**
+     * @IsGranted("ROLE_CRON")
      * @Route("/cron", methods={"GET"})
      */
     public function run(KernelInterface $kernel) {
@@ -34,8 +35,60 @@ class CronjobController extends AbstractController {
         $content = $output->fetch();
         $converter = new AnsiToHtmlConverter();
 
-        return $this->render('cron/output.html.twig', [
+        return $this->render('admin/cron/output.html.twig', [
             'output' => $converter->convert($content)
+        ]);
+    }
+
+    /**
+     * @Route("/admin/cron", name="admin_cronjobs")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function index(EntityManagerInterface $manager) {
+        $jobs = $manager->getRepository(CronJob::class)
+            ->findAll();
+
+        /** @var CronJobResult[] $results */
+        $results = [ ];
+
+        foreach($jobs as $job) {
+            $results[$job->getCommand()] = $manager->getRepository(CronJobResult::class)
+                ->findMostRecent($job);
+        }
+
+        return $this->render('admin/cron/index.html.twig', [
+            'jobs' => $jobs,
+            'results' => $results
+        ]);
+    }
+
+    /**
+     * @Route("/admin/cron/{id}", name="show_cronjob")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function showJob(CronJob $job, EntityManagerInterface $manager) {
+        $results = $manager->getRepository(CronJobResult::class)
+            ->createQueryBuilder('r')
+            ->leftJoin('r.cronJob', 'c')
+            ->where('c.id = :job')
+            ->orderBy('r.createdAt', 'desc')
+            ->setParameter('job', $job->getId())
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('cron/show.html.twig', [
+            'job' => $job,
+            'results' => $results
+        ]);
+    }
+
+    /**
+     * @Route("/admin/cron/{job}/result/{id}", name="show_cronresult")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function showResult(CronJobResult $result) {
+        return $this->render('admin/cron/result.html.twig', [
+            'result' => $result
         ]);
     }
 }

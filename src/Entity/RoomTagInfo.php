992de79ea15b3e9154024678ba5df7a54d3d7400
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-11-12 20:12:01 +0100

    Entities with composite keys cannot be audited (see https://github.com/DamienHarper/auditor-bundle/issues/57#issuecomment-483688880)

--- src/Entity/RoomTagInfo.php
+++ src/Entity/RoomTagInfo.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity()
- * @Auditable()
  * @ORM\Table(name="room_roomtags")
  */
 class RoomTagInfo {
--- src/Entity/StudyGroupMembership.php
+++ src/Entity/StudyGroupMembership.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity()
- * @Auditable()
  */
 class StudyGroupMembership {
 

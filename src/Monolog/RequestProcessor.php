edcb06795486ad68766f0f42c5e09c6b04c07885
Marcel Marnitz <marcelmarnitz@googlemail.com>
2022-11-06 19:35:16 +0100

    Log referer (important for XHR requests)

--- src/Monolog/RequestProcessor.php
+++ src/Monolog/RequestProcessor.php
@@ -9,6 +9,7 @@ class RequestProcessor implements ProcessorInterface {
 
     private ?string $userAgent = null;
     private ?string $url = null;
+    private ?string $referer = null;
 
     public function __construct(private RequestStack $requestStack)
     {
@@ -20,6 +21,7 @@ class RequestProcessor implements ProcessorInterface {
     public function __invoke(array $records): array {
         $records['extra']['useragent'] = $this->getUserAgent();
         $records['extra']['url'] = $this->getUrl();
+        $records['extra']['referer'] = $this->getXhrBaseUrl();
 
         return $records;
     }
@@ -38,5 +40,22 @@ class RequestProcessor implements ProcessorInterface {
         }
 
         return $this->url;
+    }
+
+    private function getXhrBaseUrl(): ?string {
+        if($this->referer === null && $this->requestStack->getMainRequest() !== null) {
+            $this->referer = $this->requestStack->getMainRequest()->headers->get('Referer');
+
+            // Check if referer is from same host
+            if(!empty($this->referer)) {
+                $host = parse_url($this->referer, PHP_URL_HOST);
+
+                if($host !== $this->requestStack->getMainRequest()->getHost()) {
+                    $this->referer = null;
+                }
+            }
+        }
+
+        return $this->referer;
     }
 }

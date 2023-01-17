c7396868205bbc2af5c0b77e4429969683ac2ceb
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-01-17 18:28:09 +0100

    Teacher filter may fallback to fallback section

--- src/View/Filter/TeacherFilter.php
+++ src/View/Filter/TeacherFilter.php
@@ -7,14 +7,16 @@ use App\Entity\Teacher;
 use App\Entity\User;
 use App\Entity\UserType;
 use App\Grouping\Grouper;
+use App\Repository\SectionRepositoryInterface;
 use App\Repository\TeacherRepositoryInterface;
+use App\Settings\GeneralSettings;
 use App\Sorting\Sorter;
 use App\Sorting\TeacherStrategy;
 use App\Utils\ArrayUtils;
 
 class TeacherFilter {
 
-    public function __construct(private Sorter $sorter, private TeacherRepositoryInterface $teacherRepository)
+    public function __construct(private Sorter $sorter, private TeacherRepositoryInterface $teacherRepository, private readonly GeneralSettings $generalSettings, private readonly SectionRepositoryInterface $sectionRepository)
     {
     }
 
@@ -25,6 +27,14 @@ class TeacherFilter {
 
         if($isStudentOrParent !== true && $section !== null) {
             $teachers = $this->teacherRepository->findAllBySection($section);
+
+            if(count($teachers) === 0 && $this->generalSettings->getCurrentSectionId() !== null && $section->getId() !== $this->generalSettings->getCurrentSectionId()) {
+                $section = $this->sectionRepository->findOneById($this->generalSettings->getCurrentSectionId());
+
+                if($section !== null) {
+                    $teachers = $this->teacherRepository->findAllBySection($section);
+                }
+            }
         }
 
         $teachers = ArrayUtils::createArrayWithKeys(

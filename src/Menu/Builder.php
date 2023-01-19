4301856da44f30b5199cb459f7b69fbf226a0f93
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-01-19 17:35:38 +0100

    Hide menu item (closes #298)

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -326,10 +326,12 @@ class Builder {
     public function toolsMenu(array $options = [ ]): ItemInterface {
         $root = $this->factory->createItem('root');
 
-        $root->addChild('tools.grade_teacher_intersection.label', [
-            'route' => 'grade_tuition_teachers_intersection'
-        ])
-            ->setExtra('icon', 'fas fa-random');
+        if($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            $root->addChild('tools.grade_teacher_intersection.label', [
+                'route' => 'grade_tuition_teachers_intersection'
+            ])
+                ->setExtra('icon', 'fas fa-random');
+        }
 
         return $root;
     }

6002f0fc510b04f3883a3c06c3559d32d41d9da3
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-02-17 17:31:37 +0100

    Remove faulty SQL statement

--- migrations/Version20230215092546.php
+++ migrations/Version20230215092546.php
@@ -24,13 +24,11 @@ final class Version20230215092546 extends AbstractMigration
         $this->addSql('CREATE TABLE student_learning_management_system_information (id INT UNSIGNED AUTO_INCREMENT NOT NULL, student_id INT UNSIGNED DEFAULT NULL, lms_id INT UNSIGNED DEFAULT NULL, username VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT NULL, is_consented TINYINT(1) NOT NULL, is_consent_obtained TINYINT(1) NOT NULL, is_audio_consented TINYINT(1) NOT NULL, is_video_consented TINYINT(1) NOT NULL, INDEX IDX_AAFEBB22CB944F1A (student_id), INDEX IDX_AAFEBB224FDCCE2E (lms_id), UNIQUE INDEX UNIQ_AAFEBB22CB944F1A4FDCCE2E (student_id, lms_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE student_learning_management_system_information ADD CONSTRAINT FK_AAFEBB22CB944F1A FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE student_learning_management_system_information ADD CONSTRAINT FK_AAFEBB224FDCCE2E FOREIGN KEY (lms_id) REFERENCES learning_management_system (id) ON DELETE CASCADE');
-        $this->addSql('DROP TABLE timetable_week_week');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE timetable_week_week (timetable_week_id INT UNSIGNED NOT NULL, week_id INT UNSIGNED NOT NULL, INDEX IDX_B6951BE6490F7151 (timetable_week_id), INDEX IDX_B6951BE6C86F3B2F (week_id), PRIMARY KEY(timetable_week_id, week_id)) DEFAULT CHARACTER SET utf8mb3 COLLATE `utf8mb3_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE student_learning_management_system_information DROP FOREIGN KEY FK_AAFEBB22CB944F1A');
         $this->addSql('ALTER TABLE student_learning_management_system_information DROP FOREIGN KEY FK_AAFEBB224FDCCE2E');
         $this->addSql('DROP TABLE learning_management_system');

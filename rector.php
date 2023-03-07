ef242a7076628fb353f36de50a79838710c78442
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-03-07 20:30:12 +0100

    Dependencies and rector.php updated

--- rector.php
+++ rector.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php70\Rector\FuncCall\NonVariableToVariableOnFunctionCallRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\JMSSetList;
 use Rector\Symfony\Set\SensiolabsSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
@@ -22,10 +21,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
     $rectorConfig->importNames();
-    $rectorConfig->ruleWithConfiguration(TypedPropertyRector::class, [
-        TypedPropertyRector::INLINE_PUBLIC => true
-    ]);
-
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_80
     ]);
@@ -39,10 +34,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
         SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-        SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES
-    ]);
-
-    $rectorConfig->skip([
-        NonVariableToVariableOnFunctionCallRector::class
+        SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        JMSSetList::ANNOTATIONS_TO_ATTRIBUTES
     ]);
 };

faf73d5faedb062e6b6e4a0ad5c8057638defcc6
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-05-03 08:50:33 +0200

    Fixes sorting strings

--- src/Sorting/StringStrategy.php
+++ src/Sorting/StringStrategy.php
@@ -4,7 +4,32 @@ namespace App\Sorting;
 
 class StringStrategy implements SortingStrategyInterface {
 
+    /**
+     * DIN5007, see https://de.wikipedia.org/wiki/Alphabetische_Sortierung#Deutschland
+     */
+    private const TransliterationMap = [
+        'ä' => 'ae',
+        'Ä' => 'Ae',
+        'ü' => 'ue',
+        'Ü' => 'Ue',
+        'ö' => 'oe',
+        'Ö' => 'Oe',
+        'ß' => 'ss'
+    ];
+
     public function compare($objectA, $objectB): int {
-        return strnatcasecmp((string)$objectA, (string)$objectB);
+        $objectA = str_replace(
+            array_keys(self::TransliterationMap),
+            array_values(self::TransliterationMap),
+            (string)$objectA
+        );
+
+        $objectB = str_replace(
+            array_keys(self::TransliterationMap),
+            array_values(self::TransliterationMap),
+            (string)$objectA
+        );
+
+        return strnatcasecmp($objectA, $objectB);
     }
 }

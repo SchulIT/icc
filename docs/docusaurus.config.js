db81963a3c72a4f7d9ec90df6d069eebe3138fdf
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-07-25 16:56:15 +0200

    Fixes footer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
       footer: {
         style: 'dark',
         links: [ ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} SchulIT. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,

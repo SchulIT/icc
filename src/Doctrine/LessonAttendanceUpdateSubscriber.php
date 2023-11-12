ea462f04d7e87f6303fa5478c020f9c5927683fa
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-11-12 20:35:16 +0100

    Trigger background integrity check (if enabled) after a attendance change

---
+++ src/Doctrine/LessonAttendanceUpdateSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Doctrine;
+
+use App\Entity\LessonAttendance;
+use App\Messenger\RunIntegrityCheckMessage;
+use App\Section\SectionResolverInterface;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsDoctrineListener(event: Events::postUpdate)]
+class LessonAttendanceUpdateSubscriber {
+    public function __construct(private bool $isEnabled, private readonly MessageBusInterface $messageBus, private readonly SectionResolverInterface $sectionResolver) { }
+
+    public function postUpdate(PostUpdateEventArgs $eventArgs): void {
+        if($this->isEnabled === false) {
+            return;
+        }
+
+        $entity = $eventArgs->getObject();
+
+        if($entity instanceof LessonAttendance) {
+            $section = $this->sectionResolver->getSectionForDate($entity->getEntry()->getLesson()->getDate());
+            $this->messageBus->dispatch(new RunIntegrityCheckMessage($entity->getStudent()->getId(), $section->getStart(), $section->getEnd()));
+        }
+    }
+}

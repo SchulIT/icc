6b3a6eb2df53b1d67215702bc21391f52af441c4
Marcel Marnitz <marcelmarnitz@googlemail.com>
2024-01-01 17:58:46 +0100

    Show IDs for admins (closes #431)

---
+++ src/Security/Voter/IdVoter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class IdVoter extends Voter {
+
+    public const Id = 'show-id';
+
+    public function __construct(private readonly AccessDecisionManagerInterface $accessDecisionManager) {
+
+    }
+
+    protected function supports(string $attribute, mixed $subject): bool {
+        return $attribute === self::Id;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool {
+        return $this->accessDecisionManager->decide($token, ['ROLE_ADMIN']);
+    }
+}

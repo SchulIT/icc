1d689efd82ea257568ba925bd8b5dfd0367f6ee2
Marcel Marnitz <marcelmarnitz@googlemail.com>
2024-12-22 14:27:45 +0100

    Require at least PHP 8.3

--- src/Markdown/Renderer/Block/HeadingRenderer.php
+++ src/Markdown/Renderer/Block/HeadingRenderer.php
@@ -2,16 +2,16 @@
 
 namespace App\Markdown\Renderer\Block;
 
-use League\CommonMark\Exception\InvalidArgumentException;
 use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
 use League\CommonMark\Node\Node;
 use League\CommonMark\Renderer\ChildNodeRendererInterface;
 use League\CommonMark\Renderer\NodeRendererInterface;
 use League\CommonMark\Util\HtmlElement;
+use Stringable;
 
 class HeadingRenderer implements NodeRendererInterface{
 
-    public function render(Node $node, ChildNodeRendererInterface $childRenderer) {
+    public function render(Node $node, ChildNodeRendererInterface $childRenderer): ?Stringable {
         Heading::assertInstanceOf($node);
         assert($node instanceof Heading);
 

4a9af593005d9726a524275f83179d2eb8e5cf42
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-02-06 19:26:36 +0100

    Completely use transliteration for string comparison/sorting

--- src/Sorting/StringStrategy.php
+++ src/Sorting/StringStrategy.php
@@ -4,36 +4,11 @@ namespace App\Sorting;
 
 class StringStrategy implements SortingStrategyInterface {
 
-    /**
-     * DIN5007, see https://de.wikipedia.org/wiki/Alphabetische_Sortierung#Deutschland
-     */
-    private const TransliterationMap = [
-        'ä' => 'ae',
-        'Ä' => 'Ae',
-        'ü' => 'ue',
-        'Ü' => 'Ue',
-        'ö' => 'oe',
-        'Ö' => 'Oe',
-        'ß' => 'ss'
-    ];
-
     private function transliterate(string $string): string {
         return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
     }
 
     public function compare($objectA, $objectB): int {
-        $objectA = str_replace(
-            array_keys(self::TransliterationMap),
-            array_values(self::TransliterationMap),
-            $this->transliterate((string)$objectA)
-        );
-
-        $objectB = str_replace(
-            array_keys(self::TransliterationMap),
-            array_values(self::TransliterationMap),
-            $this->transliterate((string)$objectB)
-        );
-
-        return strnatcasecmp($objectA, $objectB);
+        return strnatcasecmp($this->transliterate((string)$objectA), $this->transliterate((string)$objectB));
     }
 }

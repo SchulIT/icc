5af08523b4e6d697868ff004355ba94a68e3d2b1
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-03-23 16:22:13 +0100

    Adds return value to UpdateOrRemoveUserHandler to get some insights

--- src/Messenger/UpdateOrRemoveUserHandler.php
+++ src/Messenger/UpdateOrRemoveUserHandler.php
@@ -11,22 +11,27 @@ use SchulIT\CommonBundle\Helper\DateHelper;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-class UpdateOrRemoveUserHandler {
-    public function __construct(private readonly bool $isDebug,
-                                private readonly UserRepositoryInterface $userRepository,
-                                private readonly ClientInterface $client,
-                                private readonly DateHelper $dateHelper,
-                                private readonly LoggerInterface $logger) {
+readonly class UpdateOrRemoveUserHandler {
+
+    public function __construct(private bool                    $isDebug,
+                                private UserRepositoryInterface $userRepository,
+                                private ClientInterface         $client,
+                                private DateHelper              $dateHelper,
+                                private LoggerInterface         $logger) {
 
     }
 
-    public function __invoke(UpdateOrRemoveUserMessage $message): void {
+    public function __invoke(UpdateOrRemoveUserMessage $message): array {
         try {
             $user = $this->userRepository->findOneById($message->getUserId());
 
             if($user === null) {
                 $this->logger->notice(sprintf('UpdateOrRemoveUserMessage für Benutzer mit ID %d ignoriert. Grund: Benutzer existiert nicht mehr.', $message->getUserId()));
-                return;
+                return [
+                    'userId' => $message->getUserId(),
+                    'action' => 'ignore',
+                    'reason' => 'non_existent_user'
+                ];
             }
 
             $response = $this->client->request('GET', sprintf('/api/user/%s', $user->getIdpId()), ['verify' => $this->isDebug !== true]);
@@ -35,19 +40,38 @@ class UpdateOrRemoveUserHandler {
                 $this->logger->info(sprintf('Lösche %s, da er/sie nicht mehr im Single-Sign-On existiert.', $user->getUsername()));
                 $this->userRepository->remove($user);
 
-                return;
+                return [
+                    'userId' => $message->getUserId(),
+                    'username' => $user->getUsername(),
+                    'action' => 'remove',
+                    'reason' => 'removed_in_sso'
+                ];
             }
 
             if($response->getStatusCode() !== 200) {
                 $this->logger->error(sprintf('UpdateOrRemoveUserMessage für Benutzer mit ID %d war nicht erfolgreich: HTTP Status Code %d', $message->getUserId(), $response->getStatusCode()));
-                return;
+                return [
+                    'userId' => $message->getUserId(),
+                    'username' => $user->getUsername(),
+                    'action' => 'ignore',
+                    'reason' => 'invalid_http_status_code',
+                    'status_code' => $response->getStatusCode()
+                ];
             }
 
-            $userData = json_decode($response->getBody());
+            $json = $response->getBody()->getContents();
+            $userData = json_decode($json);
 
             if(json_last_error() !== JSON_ERROR_NONE) {
                 $this->logger->error(sprintf('UpdateOrRemoveUserMessage für Benutzer mit ID %d war nicht erfolgreich: JSON Fehler. %s', $message->getUserId(), json_last_error_msg()));
-                return;
+                return [
+                    'userId' => $message->getUserId(),
+                    'username' => $user->getUsername(),
+                    'action' => 'ignore',
+                    'reason' => 'json_error',
+                    'json' => $json,
+                    'error' => json_last_error_msg()
+                ];
             }
 
             if($userData->enabled_until !== null) {
@@ -55,7 +79,12 @@ class UpdateOrRemoveUserHandler {
                 if($enabledUntil < $this->dateHelper->getToday()) {
                     $this->logger->info(sprintf('Lösche %s, da er/sie im Single-Sign-On existiert nicht mehr aktiv ist.', $user->getUsername()));
                     $this->userRepository->remove($user);
-                    return;
+                    return [
+                        'userId' => $message->getUserId(),
+                        'username' => $user->getUsername(),
+                        'action' => 'remove',
+                        'reason' => 'not_active_in_sso'
+                    ];
                 }
             }
 
@@ -67,10 +96,23 @@ class UpdateOrRemoveUserHandler {
             $user->setEmail($userData->email);
 
             $this->userRepository->persist($user);
+
+            return [
+                'userId' => $message->getUserId(),
+                'username' => $user->getUsername(),
+                'action' => 'update'
+            ];
         } catch (Exception $e) {
             $this->logger->notice(sprintf('UpdateOrRemoveUserMessage für Benutzer mit ID %d ignoriert. Grund: %s.', $message->getUserId(), $e->getMessage()), [
                 'exception' => $e
             ]);
+
+            return [
+                'userId' => $message->getUserId(),
+                'action' => 'unknown',
+                'reason' => 'exception',
+                'exception' => $e->getMessage()
+            ];
         }
     }
 }

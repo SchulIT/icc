a33f15af220e86f38f0bf58484a678e7ebf3375f
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-12-07 15:45:27 +0100

    Autoconfig-Feature aktiviert

---
+++ src/Controller/AutoconfigController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use SchulIT\CommonBundle\Controller\AutoconfigController as BaseAutoconfigController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/autoconfig')]
+class AutoconfigController extends BaseAutoconfigController {
+
+}

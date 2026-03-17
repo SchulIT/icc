95880cdd4dce5c40241c5cc7eb7324fb1a795d47
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-03-17 18:18:20 +0100

    Consider absences with "zero absent lessons" when checking for multiple attendances

--- src/Book/IntegrityCheck/Checks/MoreThanOneAttendancesAtTheSameTimeCheck.php
+++ src/Book/IntegrityCheck/Checks/MoreThanOneAttendancesAtTheSameTimeCheck.php
@@ -4,14 +4,17 @@ namespace App\Book\IntegrityCheck\Checks;
 
 use App\Book\IntegrityCheck\IntegrityCheckInterface;
 use App\Book\IntegrityCheck\IntegrityCheckViolation;
+use App\Entity\Attendance;
+use App\Entity\AttendanceType;
 use App\Entity\Student;
 use App\Repository\LessonAttendanceRepositoryInterface;
+use App\Utils\ArrayUtils;
 use DateTime;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MoreThanOneAttendancesAtTheSameTimeCheck implements IntegrityCheckInterface {
 
-    public const Name = 'more_than_one_attendances_at_the_same_time';
+    public const string Name = 'more_than_one_attendances_at_the_same_time';
 
     public function __construct(private readonly LessonAttendanceRepositoryInterface $attendanceRepository, private readonly TranslatorInterface $translator) { }
 
@@ -20,6 +23,7 @@ class MoreThanOneAttendancesAtTheSameTimeCheck implements IntegrityCheckInterfac
 
         $attendances = $this->attendanceRepository->findByStudentAndDateRange($student, $start, $end, true);
         $lessonKeys = [ ];
+        $groupedAttendances = [ ];
 
         foreach($attendances as $attendance) {
             if($attendance->getEntry() !== null) {
@@ -32,10 +36,37 @@ class MoreThanOneAttendancesAtTheSameTimeCheck implements IntegrityCheckInterfac
 
             $lessonKey = sprintf('%s-%d', $date->format('Y-m-d'), $attendance->getLesson());
 
-            if(in_array($lessonKey, $lessonKeys)) {
+            if(!array_key_exists($lessonKey, $groupedAttendances)) {
+                $groupedAttendances[$lessonKey] = [ ];
+            }
+
+            $groupedAttendances[$lessonKey][] = $attendance;
+        }
+
+        foreach($groupedAttendances as $lessonKey => $attendances) {
+            $count = count($attendances);
+
+            if($count <= 1) {
+                continue; // only one attendance at this lesson -> PASS
+            }
+
+            $present = 0;
+            $absent = 0;
+            $absentZeroLessons = 0;
+
+            /** @var Attendance $attendance */
+            foreach($attendances as $attendance) {
+                if($attendance->getType() === AttendanceType::Present || $attendance->getType() === AttendanceType::Late) {
+                    $present++;
+                } else if($attendance->isZeroAbsentLesson()) {
+                    $absentZeroLessons++;
+                } else {
+                    $absent++;
+                }
+            }
+
+            if(($present >= 1 && $absent >= 1) || ($present >= 1 && $present === $count) || ($absent >= 1 && $absent === $count)) {
                 $violations[] = new IntegrityCheckViolation(clone $date, $attendance->getLesson(), $attendance->getEntry() !== null ? $attendance->getEntry()->getLesson() : $attendance->getEvent(), $this->translator->trans('book.integrity_check.checks.more_than_one_attendances_at_the_same_time.violation'));
-            } else {
-                $lessonKeys[] = $lessonKey;
             }
         }
 

d63a50bba3f4871317c761778e7e2514935b7944
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-06-28 13:41:15 +0200

    Combine multiple remarks into one dataset when exporting

--- src/Book/ReportRemark/Export/Schild/Exporter.php
+++ src/Book/ReportRemark/Export/Schild/Exporter.php
@@ -2,8 +2,10 @@
 
 namespace App\Book\ReportRemark\Export\Schild;
 
+use App\Book\Entity\ReportRemark;
 use App\Book\Repository\ReportRemarkRepositoryInterface;
 use App\Common\Entity\Section;
+use App\Framework\Utils\ArrayUtils;
 use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -18,7 +20,15 @@ readonly class Exporter {
     public function export(ExportRequest $request): string {
         $output = 'Nachname|Vorname|Geburtsdatum|Jahr|Abschnitt' . PHP_EOL;
 
-        foreach($this->reportRemarkRepository->findAll($request->section) as $remark) {
+        $groups = ArrayUtils::createArrayWithKeys(
+            $this->reportRemarkRepository->findAll($request->section),
+            fn(ReportRemark $reportRemark) => $reportRemark->getStudent()->getId(),
+            true
+        );
+
+        foreach($groups as $group) {
+            $remark = array_first($group);
+
             $output .= sprintf(
                 '%s|%s|%s|%d|%d',
                 $remark->getStudent()->getLastname(),
@@ -29,7 +39,10 @@ readonly class Exporter {
             ) . PHP_EOL;
 
             $output .= '<V>' . PHP_EOL;
-            $output .= $remark->getRemark() . PHP_EOL;
+
+            foreach($group as $remark) {
+                $output .= $remark->getRemark() . PHP_EOL;
+            }
             $output .= '</V>' . PHP_EOL;
         }
 
